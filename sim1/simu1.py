82ba866635feef81a2e02e5d884a7792eff83ddf
teru12012000 <sion0012@outlook.jp>
2022-09-29 17:22:54 +0900

    sim1シミュレーション完成

--- sim1/simu1.py
+++ sim1/simu1.py
@@ -2,6 +2,7 @@
 import math
 import random
 from turtle import color
+import pandas as pd
 import matplotlib.pyplot as plt
 road=1.1
 wether=1
@@ -29,24 +30,33 @@ for i in range(day_length):
 fig.append(trimp[0]*2)
 fit.append(trimp[0])
 par.append(fit[0]-fig[0])
-fig[0]*=-1
 for i in range(1,day_length):
   fig.append(2*trimp[i]+fig[i-1]*math.exp(-1/15))
   fit.append(trimp[i]+fit[i-1]*math.exp(-1/45))
   par.append(fit[i]-fig[i])
-for i in range(1,day_length):  
+for i in range(day_length):  
   fig[i]*=-1  
 
+
+'''
+print(au)
+print(distance)
 print(fit)
 print(fig)
 print(par)
-
+'''
+file_df=pd.DataFrame(zip(au,distance,fit,fig,par),columns=['au','distance','fitness','fatigue','performance'])
+figure=plt.figure()
+print(file_df)
+file_df.to_excel('./sim1.xlsx')
 plt.plot(x,fit,color='red')
 plt.plot(x,fig,color='blue')
 plt.plot(x,par,color='yellow')
 plt.xlim(1,day_length)
 plt.ylim(-500,500)
 plt.show()
+
+figure.savefig("./sim1.png")
 
 
 
