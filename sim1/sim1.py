71d6d72337a0c6b987df32b1a209461a6acbd4fb
teru12012000 <sion0012@outlook.jp>
2022-10-13 13:02:10 +0900

    sim9

--- sim1/sim1.py
+++ sim1/sim1.py
@@ -6,14 +6,24 @@ import pandas as pd
 import matplotlib.pyplot as plt
 road=1.1
 wether=1
-day_length=10
-au=[1.5,1.5,1.5,1,1,0,1,1,1,1]
-x=list(range(1,11))
-distance=[20,20,17,6,6,6,8,6,6,6]
+day_length=31
+au=[]
+x=[]
+distance=[]
 trimp=[]
 fig=[]
 fit=[]
 par=[]
+
+for i in range(day_length):
+  x.append(i+1)
+  distance.append(random.randrange(6,20))
+  sub=random.randrange(1,3)
+  if sub==1:
+    au.append(1)
+  else:
+    au.append(1.5)
+
 for i in range(day_length):
   trimp.append(round(au[i]*distance[i]*road*wether,3))
 
@@ -28,20 +38,22 @@ for i in range(day_length):
   fig[i]*=-1  
 
 
+
+
 print(f'最終日:フィットネス={fit[-1]},疲労={fig[-1]},パフォーマンス={par[-1]}')
 
 file_df=pd.DataFrame(zip(au,distance,fit,fig,par),columns=['au','distance','fitness','fatigue','performance'])
 figure=plt.figure()
 print(file_df)
-file_df.to_excel('./sim9.xlsx')
+file_df.to_excel('./sim1.xlsx')
 plt.plot(x,fit,color='red')
 plt.plot(x,fig,color='blue')
 plt.plot(x,par,color='yellow')
 plt.xlim(1,day_length)
-plt.ylim(-400,400)
+plt.ylim(-500,500)
 plt.show()
 
-figure.savefig("./sim9.png")
+figure.savefig("./sim1.png")
 
 
 
--- sim9/sim9_rand.py
+++ sim9/sim9_rand.py
@@ -6,7 +6,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 road=1.1
 wether=1
-day_length=31
+day_length=10
 au=[]
 x=[]
 distance=[]
@@ -18,9 +18,11 @@ par=[]
 for i in range(day_length):
   x.append(i+1)
   distance.append(random.randrange(6,20))
-  sub=random.randrange(1,3)
+  sub=random.randrange(1,4)
   if sub==1:
     au.append(1)
+  elif sub==2:
+    au.append(0)
   else:
     au.append(1.5)
 
@@ -38,17 +40,14 @@ for i in range(day_length):
   fig[i]*=-1  
 
 
-'''
-print(au)
-print(distance)
-print(fit)
-print(fig)
-print(par)
-'''
+
+
+print(f'最終日:フィットネス={fit[-1]},疲労={fig[-1]},パフォーマンス={par[-1]}')
+
 file_df=pd.DataFrame(zip(au,distance,fit,fig,par),columns=['au','distance','fitness','fatigue','performance'])
 figure=plt.figure()
 print(file_df)
-file_df.to_excel('./sim1.xlsx')
+file_df.to_excel('./sim9_rand.xlsx')
 plt.plot(x,fit,color='red')
 plt.plot(x,fig,color='blue')
 plt.plot(x,par,color='yellow')
@@ -56,7 +55,7 @@ plt.xlim(1,day_length)
 plt.ylim(-500,500)
 plt.show()
 
-figure.savefig("./sim1.png")
+figure.savefig("./sim9_rand.png")
 
 
 
