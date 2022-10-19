3fa98c3f9b037d839ba173f7237941fee59b6150
teru12012000 <sion0012@outlook.jp>
2022-10-19 14:11:31 +0900

    add sim9

---
+++ sim9/sim9_continue.py
@@ -0,0 +1,47 @@
+#jog onlyのシミュレーション
+import math
+import random
+from turtle import color
+import pandas as pd
+import matplotlib.pyplot as plt
+road=1.1
+wether=1
+day_length=10
+au=[1.5,1.5,1.5,1,1,0,1,1,1,1]
+x=list(range(1,11))
+distance=[20,20,16.68,6,6,6,20,8.61,6,6]
+trimp=[]
+fig=[]
+fit=[]
+par=[]
+for i in range(day_length):
+  trimp.append(round(au[i]*distance[i]*road*wether,3))
+
+fig.append(trimp[0]*2)
+fit.append(trimp[0])
+par.append(fit[0]-fig[0])
+for i in range(1,day_length):
+  fig.append(2*trimp[i]+fig[i-1]*math.exp(-1/15))
+  fit.append(trimp[i]+fit[i-1]*math.exp(-1/45))
+  par.append(fit[i]-fig[i])
+for i in range(day_length):  
+  fig[i]*=-1  
+
+
+print(f'最終日:フィットネス={fit[-1]},疲労={fig[-1]},パフォーマンス={par[-1]}')
+
+file_df=pd.DataFrame(zip(au,distance,fit,fig,par),columns=['au','distance','fitness','fatigue','performance'])
+figure=plt.figure()
+print(file_df)
+file_df.to_excel('./sim9_continue.xlsx')
+plt.plot(x,fit,color='red')
+plt.plot(x,fig,color='blue')
+plt.plot(x,par,color='yellow')
+plt.xlim(1,day_length)
+plt.ylim(-400,400)
+plt.show()
+
+figure.savefig("./sim9_continue.png")
+
+
+
